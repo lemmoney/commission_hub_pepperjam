63b0fce422e31fd6387435edba0d53d959e0f7bb
Hugo Borges - agaelebe <agaelebe@gmail.com>
2017-08-10 17:24:44 -0300

    update load order

--- lib/commission_hub_pepperjam.rb
+++ lib/commission_hub_pepperjam.rb
@@ -1,5 +1,5 @@
 require "commission_hub"
 
+require "commission_hub/pepperjam_v20120402/settings"
 require "commission_hub/pepperjam_v20120402/endpoints/base"
 require "commission_hub/pepperjam_v20120402/connection"
-require "commission_hub/pepperjam_v20120402/settings"
