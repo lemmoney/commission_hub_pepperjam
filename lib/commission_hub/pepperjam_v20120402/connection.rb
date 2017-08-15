ff014e5e58c34edd5ac8013480ae29ea9c8e5e2c
Hugo Borges - agaelebe <agaelebe@gmail.com>
2017-08-15 11:03:18 -0300

    update endpoints

--- lib/commission_hub/pepperjam_v20120402/connection.rb
+++ lib/commission_hub/pepperjam_v20120402/connection.rb
@@ -2,12 +2,12 @@ module CommissionHub
   module PepperjamV20120402
     class Connection < CommissionHub::Connection
 
-      def_endpoint :advertiser,  'advertiser',                  class: :Base
-      def_endpoint :link,        'creative/generic',            class: :Base
-      def_endpoint :coupon,      'creative/coupon',             class: :Base
-      def_endpoint :discount,    'creative/text',               class: :Base
-      def_endpoint :banner,      'creative/banner',             class: :Base
-      def_endpoint :commission,  'report/transaction-delta',    class: :Base
+      def_endpoint :advertiser,  'publisher/advertiser',               class: :Base
+      def_endpoint :link,        'publisher/creative/generic',         class: :Base
+      def_endpoint :coupon,      'publisher/creative/coupon',          class: :Base
+      def_endpoint :discount,    'publisher/creative/text',            class: :Base
+      def_endpoint :banner,      'publisher/creative/banner',          class: :Base
+      def_endpoint :commission,  'publisher/report/transaction-delta', class: :Base
 
       def initialize(settings)
         @settings = settings
