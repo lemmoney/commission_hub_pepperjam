08f2483cd6a9f5823f7b4d8a930b0de3b6d1e826
Hugo Borges - agaelebe <agaelebe@gmail.com>
2017-08-23 14:52:26 -0300

    bugfix in params

--- lib/commission_hub/pepperjam_v20120402/endpoints/base.rb
+++ lib/commission_hub/pepperjam_v20120402/endpoints/base.rb
@@ -4,12 +4,22 @@ module CommissionHub
       class Base < CommissionHub::Endpoint
 
         def call(request_params={})
-          request_params = { query: { "apiKey" => api_key }}.merge(request_params)
+          request_params = {
+              query: {
+                "apiKey" => api_key,
+                "format" => format
+              }
+            }.deep_merge(request_params)
+
           connection.class.get(full_uri, request_params)
         end
 
         def api_key
           connection.settings.api_key
+        end
+
+        def format
+          connection.settings.format
         end
 
       end
--- lib/commission_hub/pepperjam_v20120402/settings.rb
+++ lib/commission_hub/pepperjam_v20120402/settings.rb
@@ -1,7 +1,7 @@
 module CommissionHub
   module PepperjamV20120402
     class Settings
-      attr_accessor :base_uri, :api_key
+      attr_accessor :base_uri, :api_key, :format
     end
   end
 end
